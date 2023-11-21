84cc62dd7bcfdd962de442f4f0ead3160be884a5
Aditya Patel <110101372+Adi-Patel1915@users.noreply.github.com>
2023-11-21 01:25:08 -0600

    working points

--- test2.py
+++ test2.py
@@ -2,59 +2,81 @@ import pygame as game
 game.init()
 #Classes-----------
 class POINT:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.center = [self.x, self.y]
-        self.radius = 5
+    def __init__(self, center : list):
+        self.center = center
+        self.radius = 10
         self.draging = False
+        self.offsetxpos = 0
+        self.offsetypos = 0
+        self.color = (16, 12, 8)
         self.updateHitbox()
 
     def updateHitbox(self):
         self.hitbox = game.rect.Rect(self.center[0] - self.radius, self.center[1] - self.radius, self.radius*2, self.radius*2) 
 
     def Draw(self):
-        game.draw.circle(Canvas, "red", tuple(self.center), self.radius)
+        game.draw.circle(Canvas, self.color, tuple(self.center), self.radius)
+        game.draw.rect(Canvas, "light green", self.hitbox, 1)
+
+
 
 
 #------------------
 
 
-Canvas = game.display.set_mode((1920, 1200))
+Canvas = game.display.set_mode((500, 500))
 game.display.set_caption("Canvas")
-game.display.toggle_fullscreen()
-def dragChecker(Point : POINT):
-        offsetxpos, offsetypos = 0, 0
-        for event in game.event.get():
+#game.display.toggle_fullscreen()
+draggingPoint = None
+
+def dragChecker(Point : POINT, Events):
+        global draggingPoint
+        for event in Events:
             if event.type == game.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mouseXpos, mouseYpos = event.pos
                     if Point.hitbox.collidepoint(event.pos):
-                        Point.draging = True
-                        offsetxpos = Point.center[0] - mouseXpos
-                        offsetypos = Point.center[1] - mouseYpos
+                        if draggingPoint is None:
+                            draggingPoint = Point
+                            Point.draging = True
+                        Point.offsetxpos = Point.center[0] - mouseXpos
+                        Point.offsetypos = Point.center[1] - mouseYpos
             
             elif event.type == game.MOUSEBUTTONUP:
                 if event.button == 1:
+                    draggingPoint = None
                     Point.draging = False
             
             elif event.type == game.MOUSEMOTION:
                 if Point.draging:
                     mouseXpos, mouseYpos = event.pos
-                    Point.center[0] = mouseXpos + offsetxpos
-                    Point.center[1] = mouseYpos + offsetypos
+                    Point.center[0] = mouseXpos + Point.offsetxpos
+                    Point.center[1] = mouseYpos + Point.offsetypos
                     Point.updateHitbox()
 
 def main():
-    Point_test = POINT()
+    Point_test = POINT([100, 100])
+    Point_test2 = POINT([200, 200])
+    Point_test3 = POINT([300, 300])
     FPS = 144
     clock = game.time.Clock()
     switch = True
     
     while switch:
-        dragChecker(Point_test)
+        events = game.event.get()
+        for event in events:
+            if event.type == game.QUIT:
+                switch = False
+        dragChecker(Point_test, events)
+        dragChecker(Point_test2, events)
+        dragChecker(Point_test3, events)
         Canvas.fill((171, 171, 171))
         Point_test.Draw()
+        Point_test2.Draw()
+        Point_test3.Draw()
+        game.draw.aaline(Canvas, "red", tuple(Point_test.center), tuple(Point_test2.center), 1)
+        game.draw.aaline(Canvas, "red", tuple(Point_test2.center), tuple(Point_test3.center), 1)
+        game.draw.aaline(Canvas, "red", tuple(Point_test3.center), tuple(Point_test .center), 1)
         game.display.update()
         clock.tick(FPS)
     return None
